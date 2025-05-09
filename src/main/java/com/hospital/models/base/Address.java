bc4a94b1046cb433f21c7678612a3804c181d4ce
rogersatsi <rogersatsi98@gmail.com>
2025-05-09 18:58:47 +0000

    feat: add address class with getters, setters and overriden toString method

---
+++ src/main/java/com/hospital/models/base/Address.java
@@ -0,0 +1,56 @@
+package com.hospital.models.base;
+
+public class Address {
+    private int address_id;
+    private String street;
+    private String city;
+    private String country;
+
+    public Address(String street, String city, String country) {
+        this.street = street;
+        this.city = city;
+        this.country = country;
+    }
+
+    public int getAddressId() {
+        return address_id;
+    }
+
+    public void setAddressId(int address_id) {
+        this.address_id = address_id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "address_id=" + address_id +
+                ", street='" + street + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                '}';
+    }
+}
